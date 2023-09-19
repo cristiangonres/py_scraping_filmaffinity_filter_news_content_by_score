9956721f15cb99373e88db83e0c11d79aee9e7cc
cristian <gonzalez.resina@gmail.com>
2023-09-19 18:12:25 +0200

    filmaffinity_filter_news_content_by_score

--- main.py
+++ main.py
@@ -12,8 +12,13 @@ for i in prueba:
         sopa1 = bs4.BeautifulSoup(result1.text, 'lxml')
         title = sopa1.select("#main-title")[0].getText().strip()
         score = sopa1.select("#movie-rat-avg")[0].getText().strip().replace(",",".")
+        image = sopa1.select("#movie-main-image-container>a>img")[0]['src'].strip()
         if float(score) > 7.0:
-            print(f"{title} {score}")
+            print(f"{title} {score} {image}")
+            img = requests.get(image)
+            f = open(f'{title} {score}.jpg', 'wb')
+            f.write(img.content)
+            f.close()
     except IndexError or ValueError:
         continue
     
